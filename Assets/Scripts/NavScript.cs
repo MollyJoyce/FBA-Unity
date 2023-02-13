3c01a7c5a9f089ae1ea5b6d9276da99393ab87b1
Molly Joyce <mjoyce8695@student.pps.net>
2023-02-13 15:26:21 -0800

    2/13

--- Assets/Scripts/NavScript.cs
+++ Assets/Scripts/NavScript.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NavScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    List<string> Students = new List<string>();
+    List<string> NewStuInfo = new List<string>();
+
+    private string InputName;
+    //https://www.youtube.com/watch?v=guelZvubWFY
+
+    public void ReadNameInput(string Name)
     {
-        
+        InputName = Name;
+       // Debug.Log(Name);
+
+        NewStuInfo.Add(Name);
+       Debug.Log(NewStuInfo);
+
+       // return Name;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NewStuNav()
     {
-        
+        SceneManager.LoadScene("New Student Page");
     }
+
 }
