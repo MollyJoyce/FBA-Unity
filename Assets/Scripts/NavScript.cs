fbaf5396775294aa1fefb2a75b0e7b442a51c5ff
Molly Joyce <mjoyce8695@student.pps.net>
2023-02-15 15:27:53 -0800

    2/15

--- Assets/Scripts/NavScript.cs
+++ Assets/Scripts/NavScript.cs
@@ -7,21 +7,80 @@ using UnityEngine.UI;
 public class NavScript : MonoBehaviour
 {
 
-    List<string> Students = new List<string>();
-    List<string> NewStuInfo = new List<string>();
+   public List<string> Students = new List<string>();
+   public List<string> NewStuInfo = new List<string>();
 
     private string InputName;
+    private string InputGrade;
+    private string InputSchool;
+
+    public InputField NameInputField;
+    public InputField GradeInputField;
+    public InputField SchoolInputField;
     //https://www.youtube.com/watch?v=guelZvubWFY
+
+    public void Start()
+    {
+        GradeInputField.DeactivateInputField();
+        SchoolInputField.DeactivateInputField();
+    }
+
 
     public void ReadNameInput(string Name)
     {
         InputName = Name;
-       // Debug.Log(Name);
 
         NewStuInfo.Add(Name);
-       Debug.Log(NewStuInfo);
+        Debug.Log(InputName);
 
-       // return Name;
+        NameInputField.DeactivateInputField();
+        GradeInputField.ActivateInputField();
+        
+    }
+
+    public void ReadGradeInput(string Grade)
+    {
+        InputGrade = Grade;
+
+        NewStuInfo.Add(Grade);
+
+        GradeInputField.DeactivateInputField();
+        SchoolInputField.ActivateInputField();
+
+    }
+
+    public void ReadSchoolInput(string School)
+    {
+        InputSchool = School;
+
+        NewStuInfo.Add(School);
+
+    }
+
+
+    public void SubmitNewStuName(string InputName)
+    {
+        // InputName = Name;
+
+        Debug.Log(InputName);
+        NewStuInfo.Add(InputName);
+
+    }
+
+    public void SubmitNewStuGrade(string InputGrade)
+    {
+        // InputName = Grade;
+
+        NewStuInfo.Add(InputGrade);
+
+    }
+
+    public void SubmitNewStuSchool(string InputSchool)
+    {
+        // InputName = School;
+
+        NewStuInfo.Add(InputSchool);
+
     }
 
     public void NewStuNav()
